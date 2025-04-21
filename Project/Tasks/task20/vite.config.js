948574f8c73d21bdaa2d1c3922b5e64322ef8695
rmukkala <rmukkala@aum.edu>
2025-04-21 19:26:52 -0400

    Update vite.config.js

--- Project/Tasks/task20/vite.config.js
+++ Project/Tasks/task20/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-  },
+  server: {
+    port: 3000,
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy': 'frame-ancestors *'
+    }
+  }
 })
